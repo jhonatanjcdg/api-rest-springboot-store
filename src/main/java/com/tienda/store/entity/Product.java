3931a4b42933e1b829fff603f5a24f4022f4c12f
jhonatanjcdg <j3172224877@gmail.com>
2026-03-04 08:32:01 -0500

    Arreglo la versión de java de 25 a 21 por incompatibilidad

--- src/main/java/com/tienda/store/entity/Product.java
+++ src/main/java/com/tienda/store/entity/Product.java
@@ -1,10 +1,22 @@
 package com.tienda.store.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
